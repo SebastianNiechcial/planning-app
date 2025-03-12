31756c911e144bcb7e81188ddc848bf4cba8033a
Cedrones <sebastian-niechcial@wp.pl>
2025-03-12 23:31:00 +0100

    Changing names

--- src/app/home/home-page.ts
+++ src/app/home/home-page.ts
@@ -2,12 +2,12 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-home',
-  templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+  templateUrl: 'home-page.html',
+  styleUrls: ['home-page.scss'],
   standalone: false,
 })
 export class HomePage {
 
-  constructor() {}
+  constructor() { }
 
 }
